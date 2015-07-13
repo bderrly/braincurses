6a6363d6d553a3fc2edba7aefc9dc717d4076a93
Brian Derr <brian@derrclan.com>
2015-07-12 23:40:54 -0700

    Add some tests; move boilerplate to set_up/tear_down.

--- code_test.cpp
+++ code_test.cpp
@@ -2,54 +2,66 @@
 #include "test/littletest.hpp"
 
 LT_BEGIN_SUITE(code)
-	void set_up() {}
-	void tear_down() {}
+
+	Code *code;
+
+	void set_up() {
+		code = new Code(0);
+		code->createCode();
+	}
+	void tear_down() {
+		delete code;
+	}
 LT_END_SUITE(code)
 
 LT_BEGIN_AUTO_TEST(code, goodGuess)
-	Code code (0);
-	code.createCode();
-	std::vector<int> correctGuess = {2, 5, 4, 2};
+	std::vector<int> guess = {2, 5, 4, 2};
 	std::vector<int> expected = {2, 2, 2, 2};
-	std::vector<int> reply = code.isCorrect(correctGuess);
+	std::vector<int> reply = code->isCorrect(guess);
 	LT_ASSERT_COLLECTIONS_EQ(expected.begin(), expected.end(), reply.begin())
 LT_END_AUTO_TEST(goodGuess)
 
 LT_BEGIN_AUTO_TEST(code, badGuess)
-	Code code (0);
-	code.createCode();
-	std::vector<int> wrongGuess = {0, 0, 0, 0};
+	std::vector<int> guess = {0, 0, 0, 0};
 	std::vector<int> expected = {0, 0, 0, 0};
-	std::vector<int> reply = code.isCorrect(wrongGuess);
+	std::vector<int> reply = code->isCorrect(guess);
 	LT_ASSERT_COLLECTIONS_EQ(expected.begin(), expected.end(), reply.begin())
 LT_END_AUTO_TEST(badGuess)
 
 LT_BEGIN_AUTO_TEST(code, oneCorrect)
-	Code code (0);
-	code.createCode();
-	std::vector<int> wrongGuess = {2, 3, 1, 0};
+	std::vector<int> guess = {2, 3, 1, 0};
 	std::vector<int> expected = {2, 0, 0, 0};
-	std::vector<int> reply = code.isCorrect(wrongGuess);
+	std::vector<int> reply = code->isCorrect(guess);
 	LT_ASSERT_COLLECTIONS_EQ(expected.begin(), expected.end(), reply.begin())
 LT_END_AUTO_TEST(oneCorrect)
 
 LT_BEGIN_AUTO_TEST(code, oneCorrectOnePartial)
-	Code code (0);
-	code.createCode();
-	std::vector<int> wrongGuess = {2, 4, 1, 0};
+	std::vector<int> guess = {2, 4, 1, 0};
 	std::vector<int> expected = {2, 1, 0, 0};
-	std::vector<int> reply = code.isCorrect(wrongGuess);
+	std::vector<int> reply = code->isCorrect(guess);
 	LT_ASSERT_COLLECTIONS_EQ(expected.begin(), expected.end(), reply.begin())
 LT_END_AUTO_TEST(oneCorrectOnePartial)
 
-LT_BEGIN_AUTO_TEST(code, allOnes)
-	Code code (0);
-	code.createCode();
-	std::vector<int> wrongGuess = {4, 2, 2, 5};
-	std::vector<int> expected = {1, 1, 1, 1};
-	std::vector<int> reply = code.isCorrect(wrongGuess);
+LT_BEGIN_AUTO_TEST(code, twoCorrectOnePartial)
+	std::vector<int> guess = {2, 4, 1, 2};
+	std::vector<int> expected = {2, 1, 0, 2};
+	std::vector<int> reply = code->isCorrect(guess);
 	LT_ASSERT_COLLECTIONS_EQ(expected.begin(), expected.end(), reply.begin())
-LT_END_AUTO_TEST(allOnes)
+LT_END_AUTO_TEST(twoCorrectOnePartial)
+
+LT_BEGIN_AUTO_TEST(code, allPartial)
+	std::vector<int> guess = {4, 2, 2, 5};
+	std::vector<int> expected = {1, 1, 1, 1};
+	std::vector<int> reply = code->isCorrect(guess);
+	LT_ASSERT_COLLECTIONS_EQ(expected.begin(), expected.end(), reply.begin())
+LT_END_AUTO_TEST(allPartial)
+
+LT_BEGIN_AUTO_TEST(code, fuzzyInput)
+	std::vector<int> guess = {0, -1, 10293928, -2};
+	std::vector<int> expected = {0, 0, 0, 0};
+	std::vector<int> reply = code->isCorrect(guess);
+	LT_ASSERT_COLLECTIONS_EQ(expected.begin(), expected.end(), reply.begin())
+LT_END_AUTO_TEST(fuzzyInput)
 
 LT_BEGIN_AUTO_TEST_ENV()
 	AUTORUN_TESTS()
